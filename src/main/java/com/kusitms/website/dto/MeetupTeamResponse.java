43deb97c555f5f39e8dae1ad8451f23e5ebd81a2
Jeongminyooa <jmjmjmz732002@naver.com>
2023-03-03 00:58:03 +0900

    ♻️Refactor : 팀원 오름차순 정렬, 데이터 없으면 null 처리

--- src/main/java/com/kusitms/website/dto/MeetupTeamResponse.java
+++ src/main/java/com/kusitms/website/dto/MeetupTeamResponse.java
@@ -1,10 +1,10 @@
 package com.kusitms.website.dto;
 
 import com.kusitms.website.domain.project.MeetupTeam;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -17,34 +17,58 @@ public class MeetupTeamResponse {
     private List<String> android;
 
     public MeetupTeamResponse(List<MeetupTeam> team) {
-        planner = new ArrayList<>();
-        designer = new ArrayList<>();
-        frontend = new ArrayList<>();
-        backend = new ArrayList<>();
-        ios = new ArrayList<>();
-        android = new ArrayList<>();
-
         for(MeetupTeam t : team) {
             switch(t.getTeam()) {
                 case PLANNER:
+                    if(planner == null) {
+                        planner = new ArrayList<>();
+                    }
                     planner.add(t.getName());
                     break;
                 case DESIGNER:
+                    if(designer == null) {
+                        designer = new ArrayList<>();
+                    }
                     designer.add(t.getName());
                     break;
                 case FRONTEND:
+                    if(frontend == null) {
+                        frontend = new ArrayList<>();
+                    }
                     frontend.add(t.getName());
                     break;
                 case BACKEND:
+                    if(backend == null) {
+                        backend = new ArrayList<>();
+                    }
                     backend.add(t.getName());
                     break;
                 case IOS:
+                    if(ios == null) {
+                        ios = new ArrayList<>();
+                    }
                     ios.add(t.getName());
                     break;
                 case ANDROID:
+                    if(android == null) {
+                        android = new ArrayList<>();
+                    }
                     android.add(t.getName());
                     break;
             }
+        }
+
+        Collections.sort(planner);
+        Collections.sort(designer);
+        Collections.sort(backend);
+        if(frontend != null) {
+            Collections.sort(frontend);
+        }
+        if(ios != null) {
+            Collections.sort(ios);
+        }
+        if(android != null) {
+            Collections.sort(android);
         }
     }
 }
